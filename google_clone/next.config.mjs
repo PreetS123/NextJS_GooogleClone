b8bbb325946e3a1d27ed47655cd6bfc3e89421a6
PreetS123 <preetbhardwaj517@gmail.com>
2025-03-14 20:47:22 +0530

    body section created

--- google_clone/next.config.mjs
+++ google_clone/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "purepng.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
