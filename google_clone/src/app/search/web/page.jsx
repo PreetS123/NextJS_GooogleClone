ca9840f4fc04e53415e9ffe1a7b89779d1aab982
PreetS123 <preetbhardwaj517@gmail.com>
2025-03-15 14:06:00 +0530

    data fetched

--- google_clone/src/app/search/web/page.jsx
+++ google_clone/src/app/search/web/page.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const WebSearchPage = () => {
+const WebSearchPage = async ({ searchParams }) => {
+  // https://www.googleapis.com/customsearch/v1?key=INSERT_YOUR_API_KEY&cx=017576662512468239146:omuauf_lfve&q=lectures
+
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.INSERT_YOUR_API_KEY}&cx=${process.env.CONTEX_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await response.json();
+  const result = data.items;
+//   console.log("resss web", result);
+
   return (
     <div>
-      Web Search page
+      {result &&
+        result?.map((res) => {
+          return <div key={res}>{res.title}</div>;
+        })}
     </div>
-  )
-}
+  );
+};
 
 export default WebSearchPage;
