39d0cf792c2bee797f8f1b93c778bd5ec2570089
PreetS123 <preetbhardwaj517@gmail.com>
2025-03-15 12:10:18 +0530

    search box completed

--- google_clone/src/components/SearchBox.jsx
+++ google_clone/src/components/SearchBox.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFillMicFill } from "react-icons/bs";
+import { RxCross2 } from "react-icons/rx";
+import { useSearchParams, useRouter } from "next/navigation";
 
 const SearchBox = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [query, setQuery] = useState("");
+  const searchTerm = searchParams.get("searchTerm");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!query.trim()) return;
+    router.push(`/search/web?searchTerm=${query}`);
+  };
   return (
     <div>
-      
+      <form
+        onSubmit={handleSubmit}
+        className="flex border border-gray-200 px-6 py-3 mt-10 mr-5 flex-grow max-w-3xl items-center rounded-full shadow-lg hover:shadoow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl"
+      >
+        <input
+          value={query}
+          type="text"
+          className="w-full focus:outline-none"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <RxCross2
+          onClick={() => {
+            setQuery("");
+          }}
+          className="text-2xl text-gray-500 cursor-pointer sm:mr-3"
+        />
+        <BsFillMicFill className="hidden sm:inline-flex  text-4xl text-blue-500 border-l-2 border-gray-300 cursor-pointer mr-3 pl-4" />
+        <AiOutlineSearch
+          onClick={handleSubmit}
+          className="hidden text-2xl sm:inline-flex text-blue-500 cursor-pointer"
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBox
+export default SearchBox;
